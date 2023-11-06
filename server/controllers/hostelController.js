fee9d3ffc3c5c36e9b28cc0776d6fd8fabbef95c
Chung Shing <chungshingg@gmail.com>
2023-11-06 22:39:59 +0800

    Adding Full Details of restaurant and hostel MongoDB

--- server/controllers/hostelController.js
+++ server/controllers/hostelController.js
@@ -1,10 +1,12 @@
 const hostel = require('../models/hostelModel');
 
 exports.getAllHostels = (req, res) => {
-    hostel.find({ city: "Kyoto" })
-        .then((result) => {
-            res.send(result);
-        }).catch((err) => {
-            console.log(err);
-        });
+    hostel.find({
+        city: "Kyoto"
+    }).then((result) => {
+        console.log("------------- MongoDB query used: hostel.find({ city: 'Kyoto' })-------------");
+        res.send(result);
+    }).catch((err) => {
+        console.log(err);
+    });
 }

077da53b000bfac73ca1099df65190eea895465a
Chung Shing <chungshingg@gmail.com>
2023-11-17 00:18:34 +0800

    changing error

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-    res.render('index', { session: req.session });
+    res.render('home', { session: req.session });
 });
 
 app.get('/login', (req, res) => {
